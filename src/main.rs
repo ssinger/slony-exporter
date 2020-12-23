bd790bc8bdd8ef7540a56c898b1dc42d62b7facc
Steve Singer <steve@ssinger.info>
2020-12-23 14:22:47 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,110 @@
+mod slony;
+mod metrics;
+use prometheus::{Opts,Registry,Counter,TextEncoder,Encoder};
+use crate::slony::ErrorTrait;
+use tokio::prelude::*;
+use slony::{SlonyStatus,SlonyConfirm};
+use metrics::{Metrics};
+
+use std::convert::Infallible;
+
+use hyper::service::{make_service_fn,service_fn};
+use hyper::{Body,Request,Response,Server};
+#[macro_use]
+extern crate prometheus;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref METRICS: Metrics = Metrics::new();
+}
+
+async fn metric(r:Request<Body>)->Result<Response<Body>,Infallible> {
+    
+   
+    
+    let r = slony::fetch_slony_status();
+    if let Ok(ref slony) = r {
+        metric_for_connection(slony,&METRICS);
+    }
+    match r {
+
+        Ok(s) => Ok(Response::new(Body::from(render(s)))),
+        Err(x) =>  Ok(Response::builder()
+                      .status(500)
+                      .body(Body::from(String::from(x.message()))).unwrap())
+    }
+
+}
+
+#[tokio::main]
+pub async fn  main() {
+    METRICS.last_event();
+    let make_svc = make_service_fn(|_conn| {
+        async  {
+            
+            Ok::<_,Infallible>(service_fn(|request| {
+                metric(request)
+            }))
+        }
+    });
+    let addr = ([127,0,0,1],3000).into();
+    let server = Server::bind(&addr).serve(make_svc);
+    
+    server.await;
+
+}
+
+/**
+ * Creates metrics for each slony node pair.
+ *
+ */
+fn render(slony_status : SlonyStatus )->String {
+    
+    let mut buffer = vec![];
+    let encoder = TextEncoder::new();
+    let metric_family = prometheus::gather();
+    encoder.encode(&metric_family, &mut buffer).unwrap();
+    String::from_utf8(buffer).unwrap()
+}
+
+
+fn metric_for_connection(slony_connection : &SlonyStatus,metrics : &Metrics) {
+
+
+    metrics.last_event().with_label_values(&[&format!("{}",slony_connection.node_id())])
+        .set(slony_connection.last_event());
+    metrics.last_event_timestamp().with_label_values(&[&format!("{}",slony_connection.node_id())])
+        .set(slony_connection.last_event_timestamp());
+    
+    for confirm in slony_connection.confirms() {
+        if let Some(v) = confirm.last_confirmed_event {
+            metrics
+                .last_confirmed_event()
+                .with_label_values(&[&format!("{}",slony_connection.node_id()),
+                                     &format!("{}",confirm.receiver)])
+                .set(v);
+        }
+        if let Some(v) = confirm.last_confirmed_timestamp {
+            metrics.
+                last_confirmed_event_timestamp().
+                with_label_values(& [&format!("{}",slony_connection.node_id()),
+                                     &format!("{}",confirm.receiver)])
+                .set(v);
+        }
+    }
+
+    for incoming in slony_connection.incoming() {
+        metrics
+            .last_received_event()
+            .with_label_values(&[&format!("{}",slony_connection.node_id()),
+                                 &format!("{}",incoming.origin)])
+            .set(incoming.last_event_id);
+         metrics
+            .last_received_event_timestamp()
+            .with_label_values(&[&format!("{}",slony_connection.node_id()),
+                                 &format!("{}",incoming.origin)])
+            .set(incoming.last_event_timestamp);
+    }
+}
---
+++ src/slony/mod.rs
@@ -0,0 +1,218 @@
+use postgres::{Client,NoTls};
+use std::collections::{HashSet};
+
+pub struct SlonyStatus {
+
+    //The node id of this node.
+    node_id : i32,
+    confirms : Vec<SlonyConfirm>,
+    incoming: Vec<SlonyIncoming>,
+    // The last event generated on this node
+    last_event_id: i64,
+    last_event_timestamp: i64,
+}
+
+
+pub struct SlonyConfirm {
+    // #SlonyConfirms
+    // This structure tracks confirmations from the origin node
+    // to the indicated receiver.
+    
+    pub receiver: i32,
+    pub last_confirmed_event: Option<i64>,
+    pub last_confirmed_timestamp: Option<i64>,
+}
+
+pub struct SlonyIncoming {
+    //# SlonyIncoming
+    //This structure tracks incoming replication from the remote this
+    //to this node.
+    pub origin: i32,
+    pub last_event_id: i64,
+    pub last_event_timestamp: i64
+}
+
+impl SlonyStatus {
+
+    pub fn new(node_id: i32, last_event_id: i64,
+               last_event_timestamp: i64)->SlonyStatus {
+        SlonyStatus { node_id: node_id, last_event_id: last_event_id,
+                      last_event_timestamp: last_event_timestamp,
+                      confirms: vec![],
+                      incoming: vec![]}
+    }
+    pub fn node_id(&self)->i32 {
+        self.node_id
+    }
+    pub fn last_event(&self)->i64 {
+        self.last_event_id
+    }
+    pub fn last_event_timestamp(&self)->i64 {
+        self.last_event_timestamp
+    }
+    pub fn confirms(&self)->&Vec<SlonyConfirm> {
+        &self.confirms
+    }
+    pub fn incoming(&self)->&Vec<SlonyIncoming> {
+        &self.incoming
+    }
+
+
+}
+
+
+pub struct Error {
+    message : String    
+}
+
+pub trait ErrorTrait {
+    fn message(&self)->&str;
+}
+
+impl ErrorTrait for Error {
+    fn message(&self)->&str {
+        &self.message
+    }
+}
+
+impl From<postgres::Error> for Error {
+
+    fn from(error: postgres::Error)->Self {
+        Error{ message: error.to_string()}
+    }
+}
+
+
+
+fn connect(url : &str)->Result<postgres::Client,postgres::Error> {
+
+    
+    
+    Client::connect(url,NoTls)
+    
+}
+
+pub fn fetch_slony_status() -> Result<SlonyStatus,Error> {
+
+    let url_opt = std::env::var("POSTGRES_URL");
+    let url = match url_opt {
+        Ok(s)=>s,
+        Err(_e)=>return Err(Error { message: String::from("Must set POSTGRES_URL")})
+    };
+
+    let query_result = query(&url);
+    query_result
+}
+
+fn query(url : &str)->Result<SlonyStatus,Error> {
+    
+    let slony_schema_opt=std::env::var("SLONY_CLUSTER");
+    let slony_schema = match slony_schema_opt {
+        Ok(s)=> s,
+        Err(_e)=> return Err(Error{ message: String::from("SLONY_CLUSTER must be set")})
+    };
+    
+    let mut client = connect(url)?;
+
+    // Fetch information about this node from sl_status.
+    let slony_status = fetch_node_data(&mut client,&slony_schema)?;
+
+    //Fetch confirmations
+    let slony_status = fetch_node_confirmations(&mut client,&slony_schema,slony_status)?;
+
+    let slony_status = fetch_node_incoming(&mut client,&slony_schema,slony_status)?;
+    
+    //Connection leak on error?
+    client.close();
+    return Ok(slony_status)
+}
+
+fn fetch_node_data(client: &mut Client,slony_schema: &str)->Result<SlonyStatus,Error> {
+    //! # fetch_node_dat
+    //! fetch information about the node that is being monitored.
+
+    let query = format!(" select ev_origin, \
+                          ev_seqno, \
+                          extract(epoch from ev_timestamp)::int8 \
+                          from _{}.sl_event \
+                          where  \
+                          ev_origin = _{}.getLocalNodeId($1)
+                          order by ev_seqno desc limit 1",
+                        slony_schema,slony_schema);
+    let event_rows = client.query(query.as_str(),&[&format!("_{}",slony_schema)])?;
+    for row in event_rows {
+        return Ok(SlonyStatus::new(  row.get(0),
+                                     row.get(1),
+                                     row.get(2)))
+    }//for
+    
+    return Err(Error { message: String::from("No events found")})
+}
+
+fn fetch_node_confirmations(client: &mut Client, slony_schema: &str,
+                            mut slony_status:  SlonyStatus)->Result<SlonyStatus,Error> {
+
+    let query = format!("select con_received, \
+                         con_seqno, \
+                         extract(epoch from con_timestamp)::int8 \
+                         from ( \
+                              select con_received, \
+                              con_seqno, \
+                              con_timestamp, \
+                              rank() over( partition by con_origin,con_received order by con_seqno desc)
+                              from \
+                              _{}.sl_confirm \
+                              where \
+                              con_origin=$1 \
+                              ) as x \
+                          where \
+                          x.rank=1",slony_schema);
+    let confirm_rows = client.query(query.as_str(),&[&slony_status.node_id()])?;
+    let mut confirms : Vec<SlonyConfirm> = vec![];
+    for row in confirm_rows {
+        let mut confirm = SlonyConfirm{
+            receiver: row.get(0),
+            last_confirmed_event: row.get(1),
+            last_confirmed_timestamp: row.get(2)
+        };
+        confirms.push(confirm);
+    }
+    slony_status.confirms = confirms;
+    Ok(slony_status)
+}
+
+
+
+fn fetch_node_incoming(client: &mut Client, slony_schema: &str,
+                            mut slony_status:  SlonyStatus)->Result<SlonyStatus,Error> {
+    let query = format!(" select ev_origin, \
+                          ev_seqno, \
+                          extract(epoch from ev_timestamp)::int8 \
+                          from \
+                          ( select ev_origin, \
+                            ev_seqno, \
+                            ev_timestamp, \
+                            rank() over (partition by ev_origin order by ev_seqno desc) \
+                            from _{}.sl_event \
+                            where \
+                            ev_origin<>$1 \
+                            ) as x \
+                            where x.rank=1 ", slony_schema);
+    
+    let incoming_rows = client.query(query.as_str(),&[&slony_status.node_id()])?;
+    let mut incoming : Vec<SlonyIncoming> = vec![];
+    for row in incoming_rows {
+        let slony_incoming = SlonyIncoming { origin: row.get(0),
+                                             last_event_id: row.get(1),
+                                             last_event_timestamp: row.get(2)
+        };
+        incoming.push(slony_incoming);
+    }
+    slony_status.incoming = incoming;
+    Ok(slony_status)
+}
+#[cfg(test)]
+mod tests {
+    use crate::slony::{SlonyStatus, SlonyConnection};
+
+}
